306dbb3a67ac13cf6d63e00ede964c8a642b34a5
SzaronGit <szymonkuzbajob@gmail.com>
2025-11-03 01:31:59 +0100

    adding KNN model

--- recruit.py
+++ recruit.py
@@ -15,8 +15,8 @@ import numpy as np
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import cross_val_score
 
-data = pd.read_csv("task_data.csv", decimal=",")
-print(data.columns)
+data = pd.read_csv("task_data.csv", decimal=",") #float error
+
 
 X = data[[
     "Heart width", "Lung width", "CTR - Cardiothoracic Ratio",
@@ -33,3 +33,107 @@ scaler = StandardScaler()
 
 X_scaled_train = scaler.fit_transform(X_train)
 X_scaled_test = scaler.transform(X_test)
+
+#-------------------------------------------------------------------------------------------------------------
+print("K-Nearest Neighbors (KNN) Classifier")
+
+pipe_knn = Pipeline(steps=[
+    ("scaler", StandardScaler()),
+    ("model", KNeighborsClassifier(
+        n_neighbors = 3,
+        weights='distance',
+        metric='manhattan'
+    ))
+])
+
+pipe_knn.fit(X_train, y_train)
+
+cv_score = np.round(cross_val_score(pipe_knn, X_train, y_train), 2)
+
+print("Scores of training data cross-validation (each fold):")
+list(map(print, cv_score))
+print(f"\nCross-validation mean score: {np.mean(cv_score):.3}")
+print(f"Standard deviation of CV score: {np.std(cv_score):.3f}\n")
+
+
+param_grid = {
+    "model__n_neighbors": [3, 5, 7, 9, 11, 15],
+    "model__weights": ["uniform", "distance"],
+    "model__metric": ["minkowski", "manhattan", "euclidean", "chebyshev"],
+}
+
+rskf = RepeatedStratifiedKFold(
+    n_splits=5,
+    n_repeats=100,
+    random_state=None
+)
+
+pipe_knn = Pipeline(steps=[
+    ("scaler", StandardScaler()),
+    ("model", KNeighborsClassifier())
+])
+
+grid_search = GridSearchCV(
+    estimator=pipe_knn,
+    param_grid=param_grid,
+    scoring="accuracy",
+    cv=rskf,
+    verbose=1,
+    n_jobs=-1
+)
+
+grid_search.fit(X_train, y_train)
+
+print(f"Best parameters: {grid_search.best_params_}")
+print(f"Best accuracy (averaged CV): {grid_search.best_score_:.4f}\n")
+
+
+param_grid = {
+    "model__n_neighbors": [6, 7, 8],
+    "model__weights": ["uniform"],
+    "model__metric": ["manhattan"],
+}
+
+rskf = RepeatedStratifiedKFold(
+    n_splits=5,
+    n_repeats=100,
+    random_state=None
+)
+
+pipe_knn = Pipeline(steps=[
+    ("scaler", StandardScaler()),
+    ("model", KNeighborsClassifier())
+])
+
+grid_search = GridSearchCV(
+    estimator=pipe_knn,
+    param_grid=param_grid,
+    scoring="accuracy",
+    cv=rskf,
+    verbose=1,
+    n_jobs=-1
+)
+
+grid_search.fit(X_train, y_train)
+
+print(f"Best parameters: {grid_search.best_params_}")
+print(f"Best accuracy (averaged CV): {grid_search.best_score_:.4f}\n")
+
+
+pipe_knn = Pipeline(steps=[
+    ("scaler", StandardScaler()),
+    ("model", KNeighborsClassifier(
+        n_neighbors = 7,
+        weights='uniform',
+        metric='manhattan'
+    ))
+])
+
+pipe_knn.fit(X_train, y_train)
+
+cv_score = np.round(cross_val_score(pipe_knn, X_train, y_train), 2)
+
+print("Scores of training data cross-validation (each fold):")
+list(map(print, cv_score))
+print(f"\nCross-validation mean score: {np.mean(cv_score):.3}")
+print(f"Standard deviation of CV score: {np.std(cv_score):.3f}\n")

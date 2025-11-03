2689b238708764bea376c7053b0ab8447bd191de
SzaronGit <szymonkuzbajob@gmail.com>
2025-11-03 01:21:30 +0100

    data and preprocessing

--- recruit.py
+++ recruit.py
@@ -1,1 +1,35 @@
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.model_selection import GridSearchCV, RepeatedStratifiedKFold, cross_val_predict, cross_val_score, train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.naive_bayes import GaussianNB
+from sklearn.pipeline import Pipeline
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.model_selection import cross_val_score
 
+data = pd.read_csv("task_data.csv", decimal=",")
+print(data.columns)
+
+X = data[[
+    "Heart width", "Lung width", "CTR - Cardiothoracic Ratio",
+    "xx", "yy", "xy", "normalized_diff", "Inscribed circle radius",
+    "Polygon Area Ratio", "Heart perimeter", "Heart area ", "Lung area"
+]]
+
+y = data["Cardiomegaly"]
+y = data["Cardiomegaly"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+
+X_scaled_train = scaler.fit_transform(X_train)
+X_scaled_test = scaler.transform(X_test)
